f6161cfcf0ef33a96d3784532b5388fafed2b0bd
anvi <andreasgylche@gmail.com>
2024-09-04 20:49:08 +0200

    added spectre plugin

---
+++ nvim/.config/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    lazy = true,
+    cmd = { "Spectre" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.api.nvim_set_hl(0, "SpectreSearch", {})
+      vim.api.nvim_set_hl(0, "SpectreReplace", {})
+
+      require("spectre").setup({
+        highlight = {
+          search = "SpectreSearch",
+          replace = "SpectreReplace",
+        },
+        mapping = {
+          ["send_to_qf"] = {
+            map = "<C-q>",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all items to quickfix",
+          },
+        },
+        replace_engine = {
+          sed = {
+            cmd = "sed",
+            args = {
+              "-i",
+              "",
+              "-E",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
