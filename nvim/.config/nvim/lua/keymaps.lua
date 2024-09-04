4c43343e7351de4e7ddfc6e66dfbda2d81e9ea15
anvi <andreasgylche@gmail.com>
2024-09-04 21:00:16 +0200

    added more keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -4,11 +4,17 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+-- Center buffer when navigating
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "#", "#zz")
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "gd", "gdzz")
 
+-- Remove highlight
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
+-- Press 'S' for quick find/replace for the word under the cursor
 vim.keymap.set("n", "S", function()
   local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
   local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
@@ -36,3 +42,16 @@ vim.keymap.set("n", "<leader>tt", ":NeotermToggle<CR>", { silent = true })
 vim.keymap.set("n", "<leader>S", function()
   require("spectre").toggle()
 end)
+
+-- Open hover for diagnostics
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.open_float({
+    border = "rounded",
+  })
+end)
+
+vim.keymap.set("n", "<leader>ss", function()
+  require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
+    previewer = false,
+  }))
+end, { desc = "[S]earch [S]pelling suggestions" })
