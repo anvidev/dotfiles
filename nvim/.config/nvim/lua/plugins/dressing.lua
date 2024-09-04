311f1343f31e555ae1e1053e26c6abf448f77667
anvi <andreasgylche@gmail.com>
2024-09-04 20:43:09 +0200

    added border to hover

--- nvim/.config/nvim/lua/plugins/dressing.lua
+++ nvim/.config/nvim/lua/plugins/dressing.lua
@@ -3,6 +3,17 @@ return {
 		"stevearc/dressing.nvim",
 		config = function()
 			require("dressing").setup()
+
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+			})
 		end,
 	},
 }
